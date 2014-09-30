d98fde112c843244a34e84db177d37c14c740f5b
hjonline <hj_182006@hotmail.com>
2014-09-30 21:48:33 +0800

    manually remove ~/.emacs.d

--- init/set-default.el
+++ init/set-default.el
@@ -8,6 +8,7 @@
 ; 设置打开文件的缺省路径
 (setq default-directory "~/")
 (setq user-emacs-directory "E:/slkshare/emacs/.emacs.d")
+(delete-directory "~/.emacs.d")
 
 ; 让 echo-buffer 不要截断输出
 (setq eval-expression-print-length nil)
