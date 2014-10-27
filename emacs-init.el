5a4c4e1c7365bbc8a8a0c22b548d4fd690b62fa5
hj_18 <hj_182006@hotmail.com>
2014-10-27 08:12:35 +0800

    melpa.org

--- emacs-init.el
+++ emacs-init.el
@@ -82,7 +82,7 @@
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
 
