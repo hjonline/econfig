2082a1edcfb6d19b8147475d3dac3fa4fe5e2ae7
hjonline <hj_182006@hotmail.com>
2016-01-20 17:01:29 +0800

    change z68 Z68

--- emacs-init.el
+++ emacs-init.el
@@ -45,7 +45,7 @@
   (setq tramp-win-exe "~/tools/ftp.exe")
   (setq dotstardict-dir "/cygdrive/e/slkshare/emacs-setup")
   (add-to-list 'Info-default-directory-list "d:/dev/mingw/msys/1.0/local/info")))
-  (if (string= system-name "z68")
+  (if (string= system-name "Z68")
 (progn
   (setq my_dev "d:/dev")
   (setq slkshareemacs-dir "e:/slkshare/emacs")
