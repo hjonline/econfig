4a10f1ba334f32d383eb64851cfa2d14c3f76ffa
hjonline <hj_182006@hotmail.com>
2017-09-26 08:34:21 +0800

    add language environment

--- init/set-default.el
+++ init/set-default.el
@@ -5,6 +5,23 @@
 ;; (setq default-frame-alist
       ;; '((height . 39) (width . 158)))
 
+; 设置语言环境
+(set-buffer-file-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-clipboard-coding-system 'utf-8)  
+(setq ansi-color-for-comint-mode t)
+(modify-coding-system-alist 'process "*" 'utf-8)  
+(setq-default pathname-coding-system 'utf-8)  
+(prefer-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))  
+(setq locale-coding-system 'utf-8)
+(setq file-name-coding-system 'utf-8) 
+(setq default-buffer-file-coding-system 'utf-8)  
+(setq slime-net-coding-system 'utf-8-unix)
+
 ; 设置打开文件的缺省路径
 (setq default-directory "~/")
 (setq user-emacs-directory dotemacs-dir)
