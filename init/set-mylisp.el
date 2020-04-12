c6e33adfb216449e740e1247628ea459bb270ea5
hjonline <hj_182006@hotmail.com>
2020-04-12 12:55:26 +0800

    change sbcl-quicklisp

--- init/set-mylisp.el
+++ init/set-mylisp.el
@@ -2,8 +2,13 @@
 
 (setq slime-lisp-implementations
       '((clisp ("D:/dev/clisp-2.48/clisp.exe"))
-	(sbcl ("d:/dev/sbcl/sbcl.exe") :coding-system utf-8-unix)
+	(sbcl ("D:/dev/sbcl/sbcl.exe"
+	       "--core" "D:/dev/sbcl/sbcl.core")
+	      :coding-system utf-8-unix)
 	))
-
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; Replace "sbcl" with the path to your implementation
+(setq inferior-lisp-program "sbcl")
+(setq slime-default-lisp 'sbcl)
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy))
