962a8e7a5bea8263185b0374e252e46fd80389ff
hjonline <hj_182006@hotmail.com>
2020-04-12 10:42:39 +0800

    change sbcl

--- init/set-mylisp.el
+++ init/set-mylisp.el
@@ -2,7 +2,7 @@
 
 (setq slime-lisp-implementations
       '((clisp ("D:/dev/clisp-2.48/clisp.exe"))
-	(sbcl ("d:/dev/sbcl/1.1.4.0.mswin.1288-90ab477/sbcl.exe") :coding-system utf-8-unix)
+	(sbcl ("d:/dev/sbcl/sbcl.exe") :coding-system utf-8-unix)
 	))
 
 (require 'slime-autoloads)
