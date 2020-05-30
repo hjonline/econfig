8a158e4d73610e1c0bcde75aeb4bf2a055c9e990
hjonline <hj_182006@hotmail.com>
2020-05-30 08:25:09 +0800

    cancel blackboard theme

--- init/set-mytheme.el
+++ init/set-mytheme.el
@@ -2,8 +2,8 @@
 (provide 'set-mytheme)
 
 (require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-     (color-theme-initialize)
-     (load-file blackboard-theme)
-     (color-theme-blackboard)))
+;(eval-after-load "color-theme"
+;  '(progn
+;     (color-theme-initialize)
+;     (load-file blackboard-theme)
+;     (color-theme-blackboard)))
