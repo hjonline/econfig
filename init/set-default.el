4e37be3f0c246ca60d0f6f0076f0807b232f1114
hjonline <hj_182006@hotmail.com>
2022-10-15 08:10:09 +0800

    change save windows

--- init/set-default.el
+++ init/set-default.el
@@ -238,9 +238,6 @@
 ;; -- Load the saved windows automatically on boot
 (add-hook 'window-setup-hook 'resume-windows)
 
-;; -- Save place in file
-(setq-default save-place t)
-
 ;; --  Use this command to quit and save your setup
 (define-key ctl-x-map "C" 'see-you-again)
 
